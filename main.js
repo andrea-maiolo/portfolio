66b6d95be4a9b2efd3819a36cb3c91ceb872e05f
andrea-maiolo <maiolo.m.2@gmail.com>
2023-06-03 19:42:28 +0200

    changed style

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+//to make page go up on clikc of my name
+window.onload = function () {
+  // Find the <h3> element
+  var h3Element = document.querySelector("h3");
+
+  // Attach a click event listener to the <h3> element
+  h3Element.addEventListener("click", function () {
+    // Scroll the page to the top
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Optionally, you can make it scroll smoothly
+    });
+  });
+};
