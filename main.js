c894b474e90cc23122a363357c3342cbeec722a1
andrea-maiolo <maiolo.m.2@gmail.com>
2023-06-05 18:51:42 +0200

    added media query

--- main.js
+++ main.js
@@ -2,7 +2,19 @@ window.onload = function () {
   let h3Element = document.querySelector("#name");
   h3Element.addEventListener("click", function () {
     window.scrollTo({
-      top: 0,
+      top: -100,
     });
   });
 };
+
+const mobileNav = document.querySelector(".mobile-nav");
+const closeIcon = document.querySelector(".closeIcon");
+const burgerIcon = document.querySelector(".burgerIcon");
+burgerIcon.addEventListener("click", showMenu);
+
+closeIcon.addEventListener("click", showMenu);
+
+function showMenu() {
+  mobileNav.classList.toggle("closed-menu");
+  closeIcon.classList.toggle("closed-menu");
+}
