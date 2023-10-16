f7f83484d6922875c698806a2958ef81c098dd3f
andrea-maiolo <maiolo.m.2@gmail.com>
2023-10-16 18:42:38 +0200

    adding button and functionality to switch languages

--- main.js
+++ main.js
@@ -21,3 +21,21 @@ function showMenu() {
   mobileNav.classList.toggle("closed-menu");
   closeIcon.classList.toggle("closed-menu");
 }
+
+let textInEng = document.getElementsByClassName("textInEng");
+let textInIta = document.getElementsByClassName("textInIta");
+let buttonSwitch = document.querySelector("#langButton");
+
+buttonSwitch.addEventListener("click", changeLang);
+
+function changeLang(e) {
+  [...textInEng].forEach(function (element) {
+    element.classList.toggle("onDisplay");
+    element.classList.toggle("hidden");
+  });
+
+  [...textInIta].forEach(function (ele) {
+    ele.classList.toggle("hidden");
+    ele.classList.toggle("onDisplay");
+  });
+}
